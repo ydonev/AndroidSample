642cd23adbc637cce7b691c9d04df4686796989d
Yavor Donev <yavor.donev@paysafe.com>
2020-04-07 00:02:43 +0300

    Init commit

---
+++ app/src/main/java/blueprints/architecture/android/example/com/gradletest/MainActivity.kt
@@ -0,0 +1,11 @@
+package blueprints.architecture.android.example.com.gradletest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
